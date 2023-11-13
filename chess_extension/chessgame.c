9f38a8c5ee7690f02d5bc27ac3842be7eef64d2e
Tomvray <toto@DESKTOP-ED6HMEU.localdomain>
2023-11-13 08:34:56 +0100

    boards generated in chessgame

--- chess_extension/chessgame.c
+++ chess_extension/chessgame.c
@@ -1,37 +1,40 @@
 #include "chess2.h"
+#include "smallchesslib.h"
 #include "postgres.h"
 #include "fmgr.h"
 #include "utils/builtins.h"
 
 
-// void generateboards(char** boards, char *moves)
+char **generateboards(char *moves)
+{
+	SCL_Record	r;
+	SCL_Board	board;
+	SCL_recordInit(r);
+    SCL_recordFromPGN(r, moves);
+
+	int		i = 0;
+	int		nb_move = SCL_recordLength(r);
+	char 	fenString[SCL_FEN_MAX_LENGTH];
+	char	**allboards = palloc(sizeof(char *) * (nb_moves + 1));
+
+	while(i < nb_move)
+	{
+		SCL_recordApply(r, board, i);
+		SCL_boardToFEN(board, fenstring);
+		allboards[i] = pstrdup(strtok(fenString, " "));
+		i++;
+	}
+	allboards[i] = NULL; //see if needed
+	return (allboards);
+}
 
 PG_FUNCTION_INFO_V1(chessgame_in);
 Datum chessgame_in(PG_FUNCTION_ARGS)
 {
 	char		*str = PG_GETARG_CSTRING(0);
-	chessgame	*result = (chessgame *)palloc(sizeof(chessboard));
+	chessgame	*result = (chessgame *) palloc(sizeof(chessboard));
 	
 	result->moves = pstrdup(str);
-    // parser
-	//result->boards = palloc(sizeof(char *) * nb_moves);
-	//generateboards(result->boards, result->moves)
-    // SCL_Board board = SCL_BOARD_START_STATE;
-    // for chaque move get boardstate et le stocker dans une liste
-	/*int i = 0;
-	uint8_t	size;
-	char	*cb;
-
-	char * fenString = malloc(sizeof(char) * SCL_FEN_MAX_LENGTH);
-	while(i < nb_move)
-	{
-		SCL_boardMakeMove(board,SCL_SQUARE('e','4'),SCL_SQUARE('e','5'),'q');
-		size = SCL_boardToFEN(SCL_Board board, char *str);
-		cb = pstrdup(str);
-		
-        
-		boards[i] = cb;
-		i++;
-	}
-	free(str);*/
+	result->boards = generateboards(result->moves);
+	return (result);
 }
