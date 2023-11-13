545de786b40d7bcf52ef94eaa4195dd17ef65411
Wassim.AlKhouri <Wassim.Al.Khouri@ulb.be>
2023-11-13 20:31:59 +0100

    out/recv/send/ chessgame

--- chess_extension/chessgame.c
+++ chess_extension/chessgame.c
@@ -5,7 +5,7 @@
 #include "utils/builtins.h"
 
 
-char **generateboards(char *moves)
+static char **generateboards(char *moves)
 {
 	SCL_Record	r;
 	SCL_Board	board;
@@ -38,3 +38,52 @@ Datum chessgame_in(PG_FUNCTION_ARGS)
 	result->boards = generateboards(result->moves);
 	return (result);
 }
+
+PG_FUNCTION_INFO_V1(chessgame_out);
+Datum chessgame_out(PG_FUNCTION_ARGS)
+{
+	chessgame	*game = (chessgame *) PG_GETARG_POINTER(0);
+	PG_FREE_IF_COPY(game, 0);
+	PG_RETURN_CSTRING(game->moves);
+}
+
+PG_FUNCTION_INFO_V1(chessgame_recv);
+Datum chessgame_recv(PG_FUNCTION_ARGS)
+{
+	StringInfo	buf = (StringInfo) PG_GETARG_POINTER(0);
+	chessgame	*game = (chessgame *) palloc(sizeof(chessgame));
+
+	game->moves = pq_getmsgstring(buf);
+
+	SCL_Record r;
+	SCL_RecordInit(r);
+	SCL_recordFromPGN(r, game->moves);
+	int nbr_moves = SCL_recordLength(r);
+
+	game->boards = palloc(sizeof(char *) * (nbr_moves + 1));
+	for (int i = 0; i < nbr_moves; i++) 
+	{
+		game->boards[i] = pstrdup(pq_getmsgstring(buf));
+	}
+	//or we can just generate the boards from the moves
+	//game->boards = generateboards(game->moves);
+	PG_RETURN_CSTRING(game);
+}
+
+PG_FUNCTION_INFO_V1(chessgame_send);
+Datum chessgame_send(PG_FUNCTION_ARGS)
+{
+	chessgame	*game = (chessgame *) PG_GETARG_POINTER(0);
+	StringInfoData	buf;
+	pq_begintypsend(&buf);
+	pq_sendstring(&buf, game->moves);
+	int nbr_moves = SCL_recordLength(r);
+	for (int i = 0; i < nbr_moves; i++) 
+	{
+		pq_sendstring(&buf, game->boards[i]);
+	}
+	PG_FREE_IF_COPY(game, 0);
+	PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
+}
+
+
